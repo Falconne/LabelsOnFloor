d883deb710c9f5d7d9f6c6e9c7bebd5f02076445
Anuradha Dissanayake <falconne@gmail.com>
2018-06-02 16:20:12 +1200

    First commit

---
+++ Main.cs
@@ -0,0 +1,48 @@
+﻿using HugsLib.Settings;
+using HugsLib.Utils;
+using RimWorld;
+using RimWorld.Planet;
+using UnityEngine;
+using Verse;
+
+namespace RoomSense
+{
+    public class Main : HugsLib.ModBase
+    {
+
+        internal new ModLogger Logger => base.Logger;
+
+        internal static Main Instance { get; private set; }
+
+        public override string ModIdentifier => "LabelsOn";
+
+        private SettingHandle<bool> _showRoomLabels;
+
+        public Main()
+        {
+            Instance = this;
+        }
+
+        public override void OnGUI()
+        {
+            if (Current.ProgramState != ProgramState.Playing || Find.VisibleMap == null
+                || WorldRendererUtility.WorldRenderedNow)
+            {
+                return;
+            }
+
+        }
+
+        public override void WorldLoaded()
+        {
+            _infoCollector.Reset();
+        }
+
+        public override void DefsLoaded()
+        {
+            _showRoomLabels = Settings.GetHandle(
+                "showRoomLabels", "FALCLF.ShowRoomLabels".Translate(),
+                "FALCLF.ShowRoomLabelsDesc".Translate(), true);
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Labels on Floor Rimworld Mod")]
+[assembly: AssemblyDescription("Draws the names of rooms and zones on the floor")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Labels on Floor")]
+[assembly: AssemblyCopyright("Copyright © Anuradha Dissanayake  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8c45f179-0329-4b3e-a9b4-ef2ac8cedc5c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.18.1.0")]
+[assembly: AssemblyFileVersion("0.18.1.0")]
