b51e4e3cd69334601f1936abb794554a87436438
Anuradha Dissanayake <falconne@gmail.com>
2018-06-03 22:55:26 +1200

    Using Lists instead of arrays

--- LabelPlacementHandler.cs
+++ LabelPlacementHandler.cs
@@ -65,36 +65,36 @@ namespace LabelsOnFloor
 
         private Mesh CreateMeshFor(string label)
         {
-            Vector3[] vertices = new Vector3[4];
-            Vector2[] uvMap = new Vector2[4];
+            var vertices = new List<Vector3>();
+            var uvMap = new List<Vector2>();
             var size = new Vector2
             {
                 x = 0.5f,
                 y = 1f
             };
 
-            int[] triangles = new int[6];
-            vertices[0] = new Vector3(-0.5f * size.x, 0f, -0.5f * size.y);
-            vertices[1] = new Vector3(-0.5f * size.x, 0f, 0.5f * size.y);
-            vertices[2] = new Vector3(0.5f * size.x, 0f, 0.5f * size.y);
-            vertices[3] = new Vector3(0.5f * size.x, 0f, -0.5f * size.y);
+            var triangles = new List<int>();
+            vertices.Add(new Vector3(-0.5f * size.x, 0f, -0.5f * size.y));
+            vertices.Add(new Vector3(-0.5f * size.x, 0f, 0.5f * size.y));
+            vertices.Add(new Vector3(0.5f * size.x, 0f, 0.5f * size.y));
+            vertices.Add(new Vector3(0.5f * size.x, 0f, -0.5f * size.y));
 
-            uvMap[0] = new Vector2(0.030f, 0f);
-            uvMap[1] = new Vector2(0.030f, 1f);
-            uvMap[2] = new Vector2(0.015f, 1f);
-            uvMap[3] = new Vector2(0.015f, 0f);
+            uvMap.Add(new Vector2(0.030f, 0f));
+            uvMap.Add(new Vector2(0.030f, 1f));
+            uvMap.Add(new Vector2(0.015f, 1f));
+            uvMap.Add(new Vector2(0.015f, 0f));
 
-            triangles[0] = 0;
-            triangles[1] = 1;
-            triangles[2] = 2;
-            triangles[3] = 0;
-            triangles[4] = 2;
-            triangles[5] = 3;
+            triangles.Add(0);
+            triangles.Add(1);
+            triangles.Add(2);
+            triangles.Add(0);
+            triangles.Add(2);
+            triangles.Add(3);
             var mesh = new Mesh
             {
                 name = "NewPlaneMesh()",
-                vertices = vertices,
-                uv = uvMap
+                vertices = vertices.ToArray(),
+                uv = uvMap.ToArray()
             };
             mesh.SetTriangles(triangles, 0);
             mesh.RecalculateNormals();
