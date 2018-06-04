8cd3d2dcb5c711bca465907652519d55a42ad83c
Anuradha Dissanayake <falconne@gmail.com>
2018-06-04 22:36:39 +1200

    Remove unused reflection

--- LabelMaker.cs
+++ LabelMaker.cs
@@ -6,16 +6,6 @@ namespace LabelsOnFloor
 {
     public class LabelMaker
     {
-        private readonly MethodInfo _roomLabelGetter;
-
-        public LabelMaker()
-        {
-            var envInspector = GenTypes.GetTypeInAnyAssembly("EnvironmentInspectDrawer");
-
-            _roomLabelGetter =
-                envInspector?.GetMethod("GetRoomRoleLabel", BindingFlags.Static | BindingFlags.NonPublic);
-        }
-
         public string GetRoomLabel(Room room)
         {
             return room.Role.LabelCap.ToUpper();
