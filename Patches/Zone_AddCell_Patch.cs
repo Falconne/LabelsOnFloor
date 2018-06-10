b13965ad21622b01008a4ee520acf342889156ad
Anuradha Dissanayake <falconne@gmail.com>
2018-06-11 00:13:48 +1200

    Handle zone updates

---
+++ Patches/Zone_AddCell_Patch.cs
@@ -0,0 +1,15 @@
+﻿using Harmony;
+using Verse;
+
+namespace LabelsOnFloor
+{
+    [HarmonyPatch(typeof(Zone), "AddCell")]
+    public class Zone_AddCell_Patch
+    {
+        static void Postfix(ref Zone __instance)
+        {
+            Main.Instance?.LabelPlacementHandler?.AddOrUpdateZone(__instance);
+        }
+
+    }
+}
---
+++ Patches/Zone_RemoveCell_Patch.cs
@@ -0,0 +1,14 @@
+﻿using Harmony;
+using Verse;
+
+namespace LabelsOnFloor
+{
+    [HarmonyPatch(typeof(Zone), "RemoveCell")]
+    public class Zone_RemoveCell_Patch
+    {
+        static void Postfix(ref Zone __instance)
+        {
+            Main.Instance?.LabelPlacementHandler?.AddOrUpdateZone(__instance);
+        }
+    }
+}
