72fc27f5f1fd147970fbbe3b412ba74e59e6ddb3
Anuradha Dissanayake <falconne@gmail.com>
2018-06-10 23:11:36 +1200

    Add extra updator

---
+++ Patches/Room_RemoveRegion_Patch.cs
@@ -0,0 +1,15 @@
+﻿using Harmony;
+using RimWorld;
+using Verse;
+
+namespace LabelsOnFloor
+{
+    [HarmonyPatch(typeof(Room), "RemoveRegion")]
+    public class Room_RemoveRegion_Patch
+    {
+        static void Postfix(ref Room __instance)
+        {
+            Main.Instance?.LabelPlacementHandler?.SetDirtyIfAreaIsOnMap(__instance.Map);
+        }
+    }
+}
