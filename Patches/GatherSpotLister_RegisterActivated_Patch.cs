2e83ad5d3166d53f3c3e5d92ab64d16ff409a29d
Anuradha Dissanayake <falconne@gmail.com>
2018-06-10 22:23:20 +1200

    Detect dinging room changes

---
+++ Patches/GatherSpotLister_RegisterActivated_Patch.cs
@@ -0,0 +1,15 @@
+﻿using Harmony;
+using RimWorld;
+using Verse;
+
+namespace LabelsOnFloor
+{
+    [HarmonyPatch(typeof(GatherSpotLister), "RegisterActivated")]
+    public class GatherSpotLister_RegisterActivated_Patch
+    {
+        static void Postfix(ref CompGatherSpot spot)
+        {
+            Main.Instance?.LabelPlacementHandler?.SetDirty();
+        }
+    }
+}
---
+++ Patches/GatherSpotLister_RegisterDeactivated_Patch.cs
@@ -0,0 +1,15 @@
+﻿using Harmony;
+using RimWorld;
+using Verse;
+
+namespace LabelsOnFloor
+{
+    [HarmonyPatch(typeof(GatherSpotLister), "RegisterDeactivated")]
+    public class GatherSpotLister_RegisterDeactivated_Patch
+    {
+        static void Postfix(ref CompGatherSpot spot)
+        {
+            Main.Instance?.LabelPlacementHandler?.SetDirty();
+        }
+    }
+}
