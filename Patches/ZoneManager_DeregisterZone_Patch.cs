3459f577a8762a2e9f37b8cce50bf055ebdfd409
Anuradha Dissanayake <falconne@gmail.com>
2018-06-10 23:46:39 +1200

    Handle zone add/removal

---
+++ Patches/ZoneManager_DeregisterZone_Patch.cs
@@ -0,0 +1,14 @@
+﻿using Harmony;
+using Verse;
+
+namespace LabelsOnFloor
+{
+    [HarmonyPatch(typeof(ZoneManager), "DeregisterZone")]
+    public class ZoneManager_DeregisterZone_Patch
+    {
+        static void Postfix(ref Zone oldZone)
+        {
+            Main.Instance?.LabelPlacementHandler?.SetDirtyIfAreaIsOnMap(oldZone.Map);
+        }
+    }
+}
---
+++ Patches/ZoneManager_RegisterZone_Patch.cs
@@ -0,0 +1,14 @@
+﻿using Harmony;
+using Verse;
+
+namespace LabelsOnFloor
+{
+    [HarmonyPatch(typeof(ZoneManager), "RegisterZone")]
+    public class ZoneManager_RegisterZone_Patch
+    {
+        static void Postfix(ref Zone newZone)
+        {
+            Main.Instance?.LabelPlacementHandler?.SetDirtyIfAreaIsOnMap(newZone.Map);
+        }
+    }
+}
