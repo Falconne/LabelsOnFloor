62c1ed8d6f2bf55d187b97d5dc559ceb04a717b5
Anuradha Dissanayake <falconne@gmail.com>
2018-06-11 23:34:25 +1200

    Handle growing zone renaming

---
+++ Patches/Zone_Growing_SetPlantDefToGrow_Patch.cs
@@ -0,0 +1,15 @@
+﻿using Harmony;
+using RimWorld;
+
+namespace LabelsOnFloor
+{
+    [HarmonyPatch(typeof(Zone_Growing), "SetPlantDefToGrow")]
+    public class Zone_Growing_SetPlantDefToGrow_Patch
+    {
+        static void Postfix(ref Zone_Growing __instance)
+        {
+            Main.Instance?.LabelPlacementHandler?.AddOrUpdateZone(__instance);
+        }
+      
+    }
+}
