b42c1d53dca2d5b17a9f3d5566c3a5735f8e6707
Anuradha Dissanayake <falconne@gmail.com>
2018-06-16 11:24:40 +1200

    Rename [WIP]

---
+++ Designator_Rename.cs
@@ -0,0 +1,12 @@
+﻿using Verse;
+
+namespace LabelsOnFloor
+{
+    public class Designator_Rename : Designator
+    {
+        public override AcceptanceReport CanDesignateCell(IntVec3 loc)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- Resources.cs
+++ Resources.cs
@@ -7,5 +7,7 @@ namespace LabelsOnFloor
     public class Resources
     {
         public static Texture2D Font = ContentFinder<Texture2D>.Get("Consolas");
+
+        public static Texture2D Rename = ContentFinder<Texture2D>.Get("Rename");
     }
 }
